9ba5b333aeb7145898467101c3203d6a1bf1ae30
Александр Тян <76566150+GreeviltDoom@users.noreply.github.com>
2020-12-28 21:16:09 +0300

    Добавьте файлы проекта.

---
+++ ОчередиЛаба/TQueue.h
@@ -0,0 +1,88 @@
+﻿#pragma once
+template <class T>
+class TQueue 
+{
+	int first;
+	int MaxSize;
+	int Size;
+	int last;
+	T* mas;
+public:
+
+	TQueue(int _MaxSize = 20) 
+	{                   //Выделяем память
+		MaxSize = _MaxSize;
+		Size = 0;
+		mas = new T[MaxSize];
+		first = 0;
+		last = -1;
+	}
+
+	TQueue(const TQueue& tq) 
+	{                       //êîíñòðóêòîð êîïèðâîàíèÿ
+		MaxSize = tq.MaxSize;
+		Size = tq.Size;
+		mas = new T[MaxSize];
+		for (size_t i = 0; i < Size; i++) 
+		{
+			mas[i] = tq.mas[i];
+		}
+		first = tq.first;
+		last = tq.last;
+	}
+
+	bool isEmpty() 
+	{                                  //Проверка на пустоту
+		if (Size == 0) return true;
+		else return false;
+	}
+
+	bool isFull() 
+	{
+		if (Size == MaxSize) return true;             //Проверка на полноту
+		else return false;
+	}
+
+	void Push(T a) 
+	{                                  //ïîëîæèòü ýëåìåíò â î÷åðåäü
+		if (isFull()) throw 1;
+		if (last == MaxSize - 1) last = 0;
+		else {
+			last++;
+			mas[last] = a;
+			Size++;
+		}
+	}
+
+	T Pop() 
+	{                                         //âçÿòü ýëåìåíò èç î÷åðåäè
+		if (isEmpty()) throw 2;
+		T a = mas[first];
+		if (first == MaxSize - 1) first = 0;
+		else 
+		{
+			first++;
+			Size--;
+		}
+	}
+
+	T& operator = (const T& tq)
+	{                    //ïåðåãðóçêà îïåðàòîðà ïðèñâàèâàíèÿ
+		if (MaxSize != tq.MaxSize) 
+		{
+			delete[] mas;
+			MaxSize = tq.MaxSize;
+			Size = tq.Size;
+			mas = new T[MaxSize];
+		}
+		for (size_t i = 0; i < MaxSize; i++) 
+		{
+			mas[i] = tq.mas[i];
+		}
+		first = tq.first;
+		last = tq.last;
+		return *this;
+	}
+
+
+};
---
+++ ОчередиЛаба/ОчередиЛаба.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <queue> 
+#include "TQueue.h"
+
+using namespace std;
+
+int main() 
+{
+    setlocale(0, "rus");
+    queue <int> q; 
+
+    cout << "Введите 7 чисел: " << endl;
+
+    for (int h = 0; h < 7; h++) 
+    {
+        int a;
+
+        cin >> a;
+
+        q.push(a);  // добавляем в очередь элементы
+    }
+
+    cout << endl;
+    cout << "Первый элемент в очереди: " << q.front() << endl;  // выводим первый
+                                                                     // элемент очереди
+    q.pop();  // удаляем элемент из очереди
+
+    cout << "Новый первый элемент (после удаления): " << q.front() << endl;
+
+    if (!q.empty()) cout << "Очередь не пуста!";  // проверяем пуста ли очередь
+
+    system("pause");
+    return 0;
+}
